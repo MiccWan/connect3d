c7fba3b4b22b9030a8ece9bd84561e7ae7cfa5a3
MiccWan <michaelwan0906@hotmail.com>
2020-12-08 11:50:48 +0800

    dev: add socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import io from 'socket.io-client';
 import Test from 'knect-common';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let socket;
+
+  const initSocket = () => {
+    socket = io();
+
+    socket.on('msg', ({ msg }) => alert(msg));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,8 +26,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React {`Test ${Test.test}`}
+          Learn React {`${Test.test}`}
         </a>
+        <div><input type="button" value="connect" onClick={initSocket} /></div>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
+import path from 'path';
 import dotenv from 'dotenv';
 import express from 'express';
-import path from 'path';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
+const http = createServer(app);
+const io = new Server(http);
 
 // set NODE_ENV in dev mode
 if (process.env.NODE_ENV !== 'production') {
@@ -12,10 +16,15 @@ if (process.env.NODE_ENV !== 'production') {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.emit('msg', { msg: 'test msg' });
+});
+
 // host react frontend in production mode
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  
+
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
@@ -27,6 +36,6 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-})
+});
