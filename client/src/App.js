84c7e3dbc92a31b0f983fec8a558576835f9ed5e
xco1014 <61931420+xco1014@users.noreply.github.com>
2020-12-31 03:17:31 +0800

    v5

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import io from 'socket.io-client';
 import newLogger from 'knect-common/src/Logger.js';
-import logo from './logo.svg';
 import './App.css';
+import LoginPage from './container/LoginPage.js';
 
 const log = newLogger('App');
 
@@ -18,18 +18,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>client/src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LoginPage />
         <div><input type="button" value="connect" onClick={initSocket} /></div>
       </header>
     </div>
